cdd5695d3d08a82feee6a9f21e23e4aa9110b86d
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-07-14 19:48:06 +0100

    mac

--- codigo2.js
+++ codigo2.js
@@ -7,3 +7,6 @@ a = b;
 b = c;
 console.log("Ahora el valor de A " + a);
 console.log("Ahora el valor de B " + b);
+
+var a = prompt("Introduce el valor de A ", "0");
+console.log("Valor de A "  + a);
