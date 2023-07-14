ad50abb4cf77e9c56e2efe92426a6a7b71b41743
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-07-14 20:06:28 +0100

    macsi

--- codigo2.js
+++ codigo2.js
@@ -8,5 +8,14 @@ b = c;
 console.log("Ahora el valor de A " + a);
 console.log("Ahora el valor de B " + b);
 
-var a = prompt("Introduce el valor de A ", "0");
-console.log("Valor de A "  + a);
+var nomb = prompt("Introduce nombre de usuario: ", " ");
+if (nomb==="Manuel"){
+    alert("Bienvenido" + nomb)
+} else (nomb != "Manuel")
+    alert("Usuario no existe")
+
+var pass = prompt("Introduce contraseña: ", " ");
+if (pass==="2023"){
+    alert("Bienvenido" + pass)
+} else (pass != "2023")
+    alert("La Contraseña no es corecta")
