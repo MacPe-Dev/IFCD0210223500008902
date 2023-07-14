659c48985615c885fbdc2c320927019bade403c2
MacPe-Dev <proyectos.mmdev@manuelmacias.es>
2023-07-14 16:17:17 +0100

    cla

---
+++ clase.js
@@ -0,0 +1,17 @@
+
+var a = 2;
+console.log("A vale " + a);
+var b = 5;
+console.log("B vale " + b);
+c = a;
+a = b;
+b = c;
+console.log("Ahora A vale " + a);
+console.log("Ahora B vale " + b);
+
+
+
+	
+	
+    
+    
