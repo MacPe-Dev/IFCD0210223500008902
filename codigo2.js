b1fc095f71dfb0947a415ccb2ca7946fa1098279
MacPe-Dev <proyectos.mmdev@manuelmacias.es>
2023-07-17 19:07:23 +0100

    actua170723

--- codigo2.js
+++ codigo2.js
@@ -9,18 +9,6 @@ console.log("Ahora el valor de A " + a);
 console.log("Ahora el valor de B " + b);
 
 
-var nomb = prompt("Intruduce nombre de usuario: ", " ");
-if (nomb === "Manuel"){
-    // alert("Bienvenido");
-    var pass = prompt("Intruduce contraseña: ", " ");
-        if (pass === "2023"){
-            alert("Bienvenido");
-            alert("Entrada");
-   
-} else (pass !="2023");
-   alert("La contraseña no es correcta");
-    
-} else (nomb !="Manuel");
-    alert("Usuario no existe");
+
 
 
