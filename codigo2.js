c483d37eda80f4ebe7997f859c7b8c49998444ee
MacPe-Dev <139461691+MacPe-Dev@users.noreply.github.com>
2023-07-17 01:39:17 +0100

    macpe0dev

--- codigo2.js
+++ codigo2.js
@@ -8,13 +8,7 @@ b = c;
 console.log("Ahora el valor de A " + a);
 console.log("Ahora el valor de B " + b);
 
-var nomb = prompt("Introduce nombre de usuario: ", " ");
-if (nomb==="Manuel"){
-    var pass = prompt("Introduce contraseña: ", " ");
-} else if (pass==="2023"){
-    alert("Bienvenido " + nomb);
-} else(nomb != "Manuel" || pass != "2023");
-    alert("Usuario Incorrecto");
+
     
 
     
