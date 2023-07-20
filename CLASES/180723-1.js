7962b2fe16572fc3ad4dd0c67834f2e9ed1cbcb5
MacPe-Dev <proyectos.mmdev@manuelmacias.es>
2023-07-20 14:42:38 +0100

    sinc

--- CLASES/180723-1.js
+++ CLASES/180723-1.js
@@ -49,18 +49,53 @@
 //     document.write(n + "<br/>");
 //     calcular(n, 5);
 // }
-//Parámetros opcionales  toUpperCase() toLowerCase()
-//mes=="octubre" || mes=="Octubre" || mes=="OCTUBRE"
-function calcular(nro1, nro2, ver = false){
-    if(ver == false){
-    document.write("Sumar: " + (nro1+nro2) + "<br/>");
-    document.write("Restar: " + (nro1-nro2) + "<br/>");
-    document.write("Multiplicación: " + (nro1*nro2) + "<br/>");
-    document.write("División: " + (nro1/nro2) + "<br/>");
-    document.write("<hr>")
-    } else {
-        document.write("No tengo nada que mostrar!!!")
-    }
-}
-calcular(20, 5, false);
 
+//toUpperCase() toLowerCase()
+11 
+//Parámetros opcionales  
+//mes=="octubre" || mes=="Octubre" || mes=="OCTUBRE"
+// function calcular(nro1, nro2, ver = false){
+//     if(ver == false){
+//     document.write("Sumar: " + (nro1+nro2) + "<br/>");
+//     document.write("Restar: " + (nro1-nro2) + "<br/>");
+//     document.write("Multiplicación: " + (nro1*nro2) + "<br/>");
+//     document.write("División: " + (nro1/nro2) + "<br/>");
+//     document.write("<hr>")
+//     } else {
+//         document.write("No tengo nada que mostrar!!!")
+//     }
+// }
+// calcular(20, 5, false);
+
+// var edad = 18;
+// var dato = "";
+// switch (edad) {
+//   case 18:
+//     	dato = "Es mayor de edad";
+//     break;
+//   case 33:
+//  		dato = "Es joven";
+//     break;
+//   case 70:
+//  		dato = "Es mayor";
+//     break;
+//   default:
+//  		dato = "¿Cómo estás?";
+//     break;
+// }
+// console.log(dato);
+
+var edad=74;
+var nombre="Juan López";
+if(edad >= 18 && edad <= 32){
+	console.log(nombre+" tiene "+edad+" años, es mayor de edad");
+}else if(edad >= 33){
+	console.log(nombre+" tiene "+edad+" años, todavía es joven");
+}else if(edad >= 70){
+	console.log(nombre+" tiene "+edad+" años, está jubilado");
+}else {
+	console.log(nombre+" ¿Cómo te encuentras?"); 
+}
+// else{
+// 	console.log(nombre+" tiene "+edad+" años, es menor de edad");
+// }
